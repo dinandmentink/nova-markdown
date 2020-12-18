597535724da71c1b5533a9dc1dee5d5ccc3ef73c
Dinand Mentink <dinand@dcreative.nl>
2020-12-18 16:36:26 +0100

    Only register uploads routes when uploads are enabled

--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,8 @@ Route::prefix(config("nova-markdown.route-prefix"))
     ->name("nova-markdown.")
     ->group(function(){
 
-        Route::resource("uploads", UploadController::class)->only("store");
+        if(config("nova-markdown.uploads")) {
+            Route::resource("uploads", UploadController::class)->only("store");
+        }
 
     });
