5975fecd092fcb1b80673dc53de4cdb533807954
Dinand Mentink <dinand@dcreative.nl>
2020-12-18 16:30:33 +0100

    Set image upload metadata for FormField component

--- src/Markdown.php
+++ src/Markdown.php
@@ -21,4 +21,47 @@ class Markdown extends Field
      * @var \Closure|bool
      */
     public $showOnIndex = false;
+
+    /**
+     * Construct the Markdown field.
+     *
+     * This is used to set some default meta information which the FormField
+     * requires.
+     */
+
+    function __construct(
+        $name,
+        $attribute = null,
+        callable $resolveCallback = null
+    ) {
+        parent::__construct($name, $attribute);
+
+        return $this->withMeta([
+            "uploads" => config("nova-markdown.uploads"),
+            "uploadEndpoint" => route("nova-markdown.uploads.store"),
+            "maxSize" => config("nova-markdown.max-size"),
+        ]);
+    }
+
+    /**
+     * Enable or disable uploads on this field.
+     */
+
+    public function uploads(bool $enabled)
+    {
+        return $this->withMeta([
+            "uploads" => $enabled,
+        ]);
+    }
+
+    /**
+     * Set the maximum image size, in kilobytes, for this field.
+     */
+
+    public function maxSize($maxSize)
+    {
+        return $this->withMeta([
+            "maxSize" => $maxSize,
+        ]);
+    }
 }
