d05c60fc50d809949a38b08f2301ac5a446644f1
Dinand Mentink <dinand@dcreative.nl>
2020-12-18 16:28:55 +0100

    Add default configuration for nova-markdown

--- src/config/nova-markdown.php
+++ src/config/nova-markdown.php
@@ -1,5 +1,55 @@
 <?php
 
+/**
+ * Configuration for nova-markdown
+ */
+
 return [
+
+    /**
+     * Enable or disable uploads
+     */
+
+    'uploads' => env("NOVA_MARKDOWN_UPLOADS", true),
+
+    /**
+     * The path prefix that will be used by nova-markdown routes
+     */
+
+    'route-prefix' => env("NOVA_MARKDOWN_ROUTE_PREFIX", 'nova-markdown'),
+
+    /**
+     * The disk on which to store uploaded images, choose a disk as
+     * configured in config/filesystems.php.
+     */
+
+    'disk' => env("NOVA_MARKDOWN_DISK", 'public'),
+
+    /**
+     * The directory where to place uploaded images within the disk
+     */
+
+    'directory' => env("NOVA_MARKDOWN_DIRECTORY", 'uploads'),
+
+    /**
+     * The maximum size for uploaded images in kilobytes
+     */
+
+    'max-size' => env("NOVA_MARKDOWN_MAX_SIZE", 8 * 1024),
+
+    /**
+     * The maximum width for uploaded images in pixels.
+     * Uploaded images will be scaled down to this width.
+     * Use null to disable image scaling.
+     */
+
+    'max-width' => env("NOVA_MARKDOWN_MAX_WIDTH", 1920),
+
+    /**
+     * Uploaded images will be converted to this quality.
+     * Integer between 0 and 100. null for no quality adjustments.
+     */
+
+    'quality' => env("NOVA_MARKDOWN_QUALITY", 85),
 
 ];
